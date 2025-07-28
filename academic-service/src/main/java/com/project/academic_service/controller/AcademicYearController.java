525c8ce5d22e8277098fec1ba80bba9612c479f2
jovyzabel <jovymonka7@gmail.com>
2025-07-28 23:42:14 +0100

    config: properties file config

--- academic-service/src/main/java/com/project/academic_service/controller/AcademicYearController.java
+++ academic-service/src/main/java/com/project/academic_service/controller/AcademicYearController.java
@@ -1,4 +1,16 @@
 package com.project.academic_service.controller;
 
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/academics")
 public class AcademicYearController {
+    @GetMapping("/")
+    String get(){
+        return "Academic-service";
+    }
+
 }
