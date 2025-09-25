2761cf9e9ea2e100ee80424dd6d9f31d0f8a5353
oslead-solutions <oumou.vero.sow@gmail.com>
2025-09-25 10:10:32 +0000

    Environnement variable config

--- gateway-service/src/main/java/com/project/gateway_service/GatewayServiceApplication.java
+++ gateway-service/src/main/java/com/project/gateway_service/GatewayServiceApplication.java
@@ -13,9 +13,37 @@ import java.util.Objects;
 
 public class GatewayServiceApplication {
 
-	public static void main(String[] args) {
+	/*public static void main(String[] args) {
 		Dotenv dotenv = Dotenv.load();
 		System.setProperty("JWT_SECRET_KEY", Objects.requireNonNull(dotenv.get("JWT_SECRET_KEY")));
+		SpringApplication.run(GatewayServiceApplication.class, args);
+	}*/
+	public static void main(String[] args) {
+		String jwtSecretKey;
+
+		try {
+			// Tentative de lecture via .env (utile en local)
+			Dotenv dotenv = Dotenv.load();
+			jwtSecretKey =  Objects.requireNonNull(dotenv.get("JWT_SECRET_KEY"));
+
+			System.out.println("Chargement des variables depuis .env ou System.getenv() terminé ");
+
+		} catch (Exception e) {
+			// fallback en cas de souci avec dotenv
+			System.out.println(" Impossible de charger .env, on utilise uniquement System.getenv()");
+
+			jwtSecretKey = System.getenv("JWT_SECRET_KEY");
+		}
+
+		// Vérification des valeurs
+		if ( jwtSecretKey == null) {
+			throw new IllegalStateException("Variables manquantes (JWT_SECRET_KEY)");
+		}
+
+		// Injection dans Spring
+
+		System.setProperty("JWT_SECRET_KEY", jwtSecretKey);
+
 		SpringApplication.run(GatewayServiceApplication.class, args);
 	}
 
